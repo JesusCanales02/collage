86ace13c994a407256d599a8e93b9619db8a5ba3
JesusCanales02 <jadrian4b@gmail.com>
2024-10-22 19:55:51 -0700

    Instruccion 2

--- src/main/java/mx/edu/cetys/jesus/canales/code_collage/repository/DogRepository.java
+++ src/main/java/mx/edu/cetys/jesus/canales/code_collage/repository/DogRepository.java
@@ -3,6 +3,7 @@ import mx.edu.cetys.jesus.canales.code_collage.model.Dog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+//Es necesario este dogreposity para las inyecciones
 @Repository
 public interface DogRepository extends JpaRepository<Dog, Long> {
 
